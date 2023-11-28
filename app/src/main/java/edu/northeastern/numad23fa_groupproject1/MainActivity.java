97c3a83fe90294be7abcb1e26d771041a90844d2
silvia yu <silviasyu@gmail.com>
2023-11-27 22:31:50 -0500

    created the homepage

--- app/src/main/java/edu/northeastern/numad23fa_groupproject1/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23fa_groupproject1/MainActivity.java
@@ -2,7 +2,10 @@ package edu.northeastern.numad23fa_groupproject1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: remove this button; this is a dummy placement button that opens the homepage activity
+        Button button = findViewById(R.id.dummyButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homePageIntent = new Intent(MainActivity.this, HomepageActivity.class);
+                startActivity(homePageIntent);
+            }
+        });
     }
 }
