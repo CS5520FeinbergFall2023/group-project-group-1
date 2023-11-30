7008f2ec19f396ac98d56fb0fdbe81e157fa1250
Rishabh0896 <rishabhdinesh.gupta@gmail.com>
2023-11-30 08:18:40 -0500

    Adding components and functionality

--- app/src/main/java/edu/northeastern/numad23fa_groupproject1/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23fa_groupproject1/MainActivity.java
@@ -2,13 +2,23 @@ package edu.northeastern.numad23fa_groupproject1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button quizButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        quizButton = findViewById(R.id.QuizButton);
+        quizButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, QuizActivity.class);
+            intent.putExtra("USER_NAME", "testUser");
+            startActivity(intent);
+        });
     }
 }
