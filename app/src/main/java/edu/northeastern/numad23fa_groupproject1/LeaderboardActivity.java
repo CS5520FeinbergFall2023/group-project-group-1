8079ddb2a98d0068dd475ba62c992864c8bc1dda
adityaputtigampala <aditya.puttigampala@gmail.com>
2023-12-03 10:55:29 -0500

    create leaderboard activity with layout

---
+++ app/src/main/java/edu/northeastern/numad23fa_groupproject1/LeaderboardActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad23fa_groupproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LeaderboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+    }
+}
